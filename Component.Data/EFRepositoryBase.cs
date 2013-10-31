e38ac676688e839506359662383e6b1b91612fec
lingsu <q25a25q@live.com>
2013-10-31 21:37:58 +0800

    a

---
+++ Component.Data/EFRepositoryBase.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.ComponentModel.Composition;
+using System.Data.Entity;
+
+using Component.Tools;
+
+namespace Component.Data
+{
+    public abstract class EFRepositoryBase<TEntity>:IRepository<TEntity> where TEntity:EntityBase
+    {
+        /// <summary>
+        /// 获取 仓储上下文的实例
+        /// </summary>
+        [Import]
+        public IUnitOfWork UnitOfWork { get; set; }
+        protected IUnitOfWorkContext EFContext
+        {
+            get
+            {
+                if (UnitOfWork is IUnitOfWorkContext)
+                {
+                    return UnitOfWork as IUnitOfWorkContext;
+                }
+                throw new DataAccessException(string.Format("数据仓储上下文对象类型不正确，应为IUnitOfWorkContext，实际为 {0}", UnitOfWork.GetType().Name));
+            }
+        }
+        public virtual IQueryable<TEntity> Entities
+        {
+            get { return EFContext.Set<TEntity>(); }
+        }
+
+        public int Insert(TEntity entity, bool isSave = true)
+        {
+             EFContext.RegisterNew(entity);
+             return isSave ? EFContext.Commit() : 0;
+        }
+
+        public int Insert(IEnumerable<TEntity> entities, bool isSave = true)
+        {
+            EFContext.RegisterNew(entities);
+            return isSave ? EFContext.Commit() : 0;
+        }
+
+        public int Delete(object id, bool isSave = true)
+        {
+            TEntity entity = EFContext.Set<TEntity>().Find(id);
+            return entity != null ? Delete(entity) : 0;
+        }
+
+        public int Delete(TEntity entity, bool isSave = true)
+        {
+            EFContext.RegisterDeleted(entity);
+            return isSave ? EFContext.Commit() : 0;
+        }
+
+        public int Delete(IEnumerable<TEntity> entities, bool isSave = true)
+        {
+            EFContext.RegisterDeleted(entities);
+            return isSave ? EFContext.Commit() : 0;
+        }
+
+        public int Delete(Expression<Func<TEntity, bool>> predicate, bool isSave = true)
+        {
+            List<TEntity> entities = EFContext.Set<TEntity>().Where(predicate).ToList();
+            return entities.Count > 0 ? Delete(entities) : 0;
+        }
+
+        public int Update(TEntity entity, bool isSave = true)
+        {
+            EFContext.RegisterModified(entity);
+            return isSave ? EFContext.Commit() : 0;
+        }
+        public int Update(IEnumerable<TEntity> entities, bool isSave = true)
+        {
+            EFContext.RegisterModified(entities);
+            return isSave ? EFContext.Commit() : 0;
+        }
+
+        public TEntity GetByKey(object key)
+        {
+            return EFContext.Set<TEntity>().Find(key);
+        }
+    }
+}
---
+++ Component.Data/UnitOfWorkContextBase.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+
+using Component.Tools;
+
+namespace Component.Data
+{
+    /// <summary>
+    /// 单元操作实现
+    /// </summary>
+    public abstract class UnitOfWorkContextBase : IUnitOfWorkContext
+    {
+        /// <summary>
+        /// 获取 当前使用的数据访问上下文对象
+        /// </summary>
+        protected abstract DbContext Context { get; }
+        public DbSet<TEntity> Set<TEntity>() where TEntity : EntityBase
+        {
+            return Context.Set<TEntity>();
+        }
+
+        public void RegisterNew<TEntity>(TEntity entity) where TEntity : EntityBase
+        {
+            EntityState state = Context.Entry(entity).State;
+            if (state == EntityState.Detached)
+            {
+                Context.Entry(entity).State = EntityState.Added;
+            }
+            IsCommitted = false;
+        }
+
+        public void RegisterNew<TEntity>(IEnumerable<TEntity> entities) where TEntity : EntityBase
+        {
+
+            try
+            {
+                Context.Configuration.AutoDetectChangesEnabled = false;
+                foreach (var item in entities)
+                {
+                    RegisterNew(item);
+                }
+            }
+            finally
+            {
+                Context.Configuration.AutoDetectChangesEnabled = true;
+            }
+        }
+
+        public void RegisterModified<TEntity>(TEntity entity) where TEntity : EntityBase
+        {
+            if (Context.Entry(entity).State == EntityState.Modified)
+            {
+                Context.Set<TEntity>().Attach(entity);
+            }
+            Context.Entry(entity).State = EntityState.Modified;
+            IsCommitted = false;
+        }
+        public void RegisterModified<TEntity>(IEnumerable<TEntity> entities) where TEntity : EntityBase
+        {
+            try
+            {
+                Context.Configuration.AutoDetectChangesEnabled = false;
+                foreach (var item in entities)
+                {
+                    RegisterModified(item);
+                }
+            }
+            finally
+            {
+                Context.Configuration.AutoDetectChangesEnabled = true;
+            }
+        }
+
+        public void RegisterDeleted<TEntity>(TEntity entity) where TEntity : EntityBase
+        {
+            Context.Entry(entity).State = EntityState.Deleted;
+            IsCommitted = false;
+        }
+
+        public void RegisterDeleted<TEntity>(IEnumerable<TEntity> entities) where TEntity : EntityBase
+        {
+            try
+            {
+                Context.Configuration.AutoDetectChangesEnabled = false;
+                foreach (var item in entities)
+                {
+                    RegisterDeleted(item);
+                }
+            }
+            finally
+            {
+                Context.Configuration.AutoDetectChangesEnabled = true;
+            }
+        }
+
+        /// <summary>
+        /// 获取 当前单元操作是否已被提交
+        /// </summary>
+        public bool IsCommitted
+        {
+            get;
+            private set;
+        }
+
+        public int Commit()
+        {
+            if (IsCommitted)
+            {
+                return 0;
+            }
+            try
+            {
+                int result = Context.SaveChanges();
+                IsCommitted = true;
+                return result;
+            }
+            catch (DbUpdateException e)
+            {
+                if (e.InnerException != null && e.InnerException.InnerException is SqlException)
+                {
+                    SqlException sqlEx = e.InnerException.InnerException as SqlException;
+                    //string msg = DataHelper.GetSqlExceptionMessage(sqlEx.Number);
+                    //throw PublicHelper.ThrowDataAccessException("提交数据更新时发生异常：" + msg, sqlEx);
+                }
+                throw;
+            }
+        }
+        /// <summary>
+        /// 把当前单元操作回滚成未提交状态
+        /// </summary>
+        public void Rollback()
+        {
+            IsCommitted = false;
+        }
+
+        public void Dispose()
+        {
+            if (!IsCommitted)
+            {
+                Commit();
+            }
+            Context.Dispose();
+        }
+    }
+}
